cd173d672868105c6ab5a0e510f192c42b4d3b59
Ahmed Saba <masyoung18@gmail.com>
2021-10-18 19:14:39 +0100

    project setup

---
+++ comps/Navbar.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+            </Head>
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      
+      <h1>Home page</h1>
+    </div>
+  )
+}
